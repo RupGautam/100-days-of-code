e4c78a036af7ddb981e7b80e00a8c7a9117e1834
Rup Gautam <rupngautam@gmail.com>
2017-07-24 17:49:10 -0500

    starting off with 100daysofjava

---
+++ src/test.java
@@ -0,0 +1,12 @@
+/**
+ * let's get started. 24th July.
+ * 100 days of java.
+ */
+public class test {
+
+  public static void main(String args[]){
+
+
+    System.out.println("Hello World!, Let's code!!");
+  }
+}
